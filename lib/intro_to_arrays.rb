2fdbbeeb63cfece3275c0f341b6b9fb871c55122
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-31 02:25:18 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+def instantiate_new_array
+  instantiate_new_array = []
+end  
+
+def array_with_two_elements
+  array_with_two_elements = ["1", "2"]
+end  
+
+def first_element(anything)
+  anything[0]
+end  
+
+def third_element(anything)
+  anything[2]
+end  
+
+def last_element(anything)
+  anything.last
+end 
+
+def first_element_with_array_methods(whatever)
+ whatever.first
+end
+
+def last_element_with_array_methods(whatever)
+  whatever.last
+end 
+
+def length_of_array(something)
+  something.length
+end  
+
+
+
+
